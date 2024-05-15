cbee9c91cda6d9e74fe13c86ced5e373592e3564
SunJiHoon <cangogi1@naver.com>
2024-05-15 12:37:28 +0900

    프로젝트 세팅

---
+++ EchoPangPang/EchoPangPang/src/main/java/hackathon/EchoPangPang/EchoPangPangApplication.java
@@ -0,0 +1,13 @@
+package hackathon.EchoPangPang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoPangPangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoPangPangApplication.class, args);
+	}
+
+}
---
+++ EchoPangPang/EchoPangPang/src/test/java/hackathon/EchoPangPang/EchoPangPangApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.EchoPangPang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoPangPangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
