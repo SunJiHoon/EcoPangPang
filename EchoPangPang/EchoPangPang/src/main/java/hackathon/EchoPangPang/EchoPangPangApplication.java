dcc67e9a550a5f014bc96c864e4f647c5af0ad04
SunJiHoon <cangogi1@naver.com>
2024-05-16 21:40:29 +0900

    loginPage완성

--- EchoPangPang/EchoPangPang/src/main/java/hackathon/EchoPangPang/EchoPangPangApplication.java
+++ EchoPangPang/EchoPangPang/src/main/java/hackathon/EchoPangPang/EchoPangPangApplication.java
@@ -2,12 +2,32 @@ package hackathon.EchoPangPang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EchoPangPangApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EchoPangPangApplication.class, args);
+	}
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:20007", "https://ecopuangpuang.putiez.com"
+								, "http://m.tongtongtripmap.com", "https://m.tongtongtripmap.com")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION")
+						.allowCredentials(true).allowedHeaders("*");//;;
+
+				////.allowedHeaders("*");;
+			}
+		};
 	}
 
 }
