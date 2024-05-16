3758007b3feafcdc64cba226c911c186bcc2c657
Just-love-myself <ehrbs7181@gmail.com>
2024-05-16 22:20:12 +0900

    #8 feat : Member 엔티티에 Puang객체 추가

---
+++ EchoPangPang/EchoPangPang/src/main/java/hackathon/EchoPangPang/entity/Puang.java
@@ -0,0 +1,28 @@
+package hackathon.EchoPangPang.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+public class Puang {
+
+    public enum Grade {
+        EGG,        //알
+        BABY,       //신생아
+        CHILD,      //유아
+        TEENAGER,   //청소년
+        ADULT,       //성인
+        GUARDIAN    //수호자
+    }
+
+    @Column(name = "puang_name")
+    private String name;
+
+    @Column(name = "puang_grade")
+    private Grade grade;
+
+}
