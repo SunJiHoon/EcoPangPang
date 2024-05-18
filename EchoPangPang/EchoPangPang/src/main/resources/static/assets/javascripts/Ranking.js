8aa11098576ff394e1b994c33cb1e62acf017db1
SunJiHoon <cangogi1@naver.com>
2024-05-18 18:48:01 +0900

    랭킹 js 구현

--- EchoPangPang/EchoPangPang/src/main/resources/static/assets/javascripts/Ranking.js
+++ EchoPangPang/EchoPangPang/src/main/resources/static/assets/javascripts/Ranking.js
@@ -1,7 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    renderInitialRankings();
+});
+
+function renderInitialRankings() {
+    fetch('/api/Ranking/Sort')
+        .then(response => response.json())
+        .then(data => {
+            displayResults(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('데이터를 가져오는 중 오류가 발생했습니다.');
+        });
+}
+
 function search() {
     var query = document.getElementById('searchInput').value;
     if (query) {
-        fetch(`/api/Ranking/search?q=${encodeURIComponent(query)}`)
+        // fetch(`/api/Ranking/search?q=${encodeURIComponent(query)}`)
+        fetch(`/api/Ranking/Search/${encodeURIComponent(query)}`)
             .then(response => response.json())
             .then(data => {
                 displayResults(data);
@@ -19,11 +36,11 @@ function displayResults(data) {
     var resultsContainer = document.getElementById('results');
     resultsContainer.innerHTML = ''; // 기존 결과를 초기화
 
-    if (data.results && data.results.length > 0) {
-        data.results.forEach(result => {
+    if (data && data.length > 0) {
+        data.forEach(result => {
             var resultElement = document.createElement('div');
             resultElement.classList.add('result');
-            resultElement.textContent = result.title; // 결과의 제목 표시 (적절히 수정 필요)
+            resultElement.textContent = `이름: ${result.name}, 포인트: ${result.point}, 등급: ${result.puangGrade}`; // 결과의 제목 표시 (적절히 수정 필요)
             resultsContainer.appendChild(resultElement);
         });
     } else {
