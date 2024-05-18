5f294eab5333fefa8f08d52d65ffc0ffdad4e7b6
Just-love-myself <ehrbs7181@gmail.com>
2024-05-19 01:05:04 +0900

    #70 feat : @Scheduled 적용 기능 완성

--- EchoPangPang/EchoPangPang/src/main/java/hackathon/EchoPangPang/EchoPangPangApplication.java
+++ EchoPangPang/EchoPangPang/src/main/java/hackathon/EchoPangPang/EchoPangPangApplication.java
@@ -5,33 +5,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableJpaAuditing
+@EnableScheduling
 public class EchoPangPangApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EchoPangPangApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EchoPangPangApplication.class, args);
+    }
 
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:20007", "https://ecopuangpuang.putiez.com"
-								, "https://ecopuang.putiez.com"
-								, "http://m.tongtongtripmap.com", "https://m.tongtongtripmap.com")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION")
-						.allowCredentials(true).allowedHeaders("*");//;;
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:20007", "https://ecopuangpuang.putiez.com"
+                                , "https://ecopuang.putiez.com"
+                                , "http://m.tongtongtripmap.com", "https://m.tongtongtripmap.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION")
+                        .allowCredentials(true).allowedHeaders("*");//;;
 
-				////.allowedHeaders("*");;
-			}
-		};
-	}
+                ////.allowedHeaders("*");;
+            }
+        };
+    }
 
 }
