e0b997a255126aef4950eb50a35153f3ba9d5a25
givemethatsewon <nvpz1598@cau.ac.kr>
2024-05-18 23:55:08 +0900

    #54 feat: 이메일 코드 인증 구현

--- EchoPangPang/EchoPangPang/src/main/java/hackathon/EchoPangPang/controller/EmailController.java
+++ EchoPangPang/EchoPangPang/src/main/java/hackathon/EchoPangPang/controller/EmailController.java
@@ -18,11 +18,11 @@ import java.util.Map;
 public class EmailController {
     private final EmailService emailService;
 
-
     /**
      * 이메일 전송 요청을 처리하는 엔드포인트.
      **/
-    @PostMapping("/api/sendEmail")
+    @PostMapping("/api/SendEmail")
+    @ResponseBody
     public ResponseEntity<Map<String, String>> sendEmail(@RequestBody Map<String, String> requestMap) {
         String to = requestMap.get("to");
         Map<String, String> response = new HashMap<>();
@@ -34,6 +34,27 @@ public class EmailController {
             response.put("message", "Error sending email: " + e.getMessage());
             return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
     }
+
+    @PostMapping("/api/CheckCode")
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> checkCode(@RequestBody Map<String, String> requestMap) {
+        String code = requestMap.get("code");
+        String email = requestMap.get("email");
+
+        Map<String, String> response = new HashMap<>();
+        try {
+            boolean isChecked = emailService.checkCode(code, email);
+            if (isChecked) {
+                response.put("result", "success");
+            } else {
+                response.put("result", "fail");
+            }
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (MessagingException e) {
+            response.put("message", "Error checking code: " + e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
